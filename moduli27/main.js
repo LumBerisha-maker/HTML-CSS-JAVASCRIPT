04e2e4b0c9d20f68b4a7c86a6a549467a088cb92
LumBerisha-maker <lumberisha12@outlook.com>
2025-09-25 20:45:56 +0200

    moduli27

---
+++ moduli27/main.js
@@ -0,0 +1,27 @@
+function lum (){
+
+   alert("this is inside the function") 
+}
+
+lum()
+
+function buna(number1, number2) {
+
+    return number1+number2;
+
+}
+
+console.log(buna(10,10))
+
+function toCelsius (number1){
+  return number1*5/9
+}
+
+console.log ("54 fahrenheit ="+toCelsius(54)+" celsius")
+
+function drin(){
+    var localVar="digitalschool";
+   alert(localVar) 
+}
+
+drin()
