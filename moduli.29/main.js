fbee8efb03a9ae81053b963c4ba62c7e0a7c54d4
LumBerisha-maker <lumberisha12@outlook.com>
2025-10-09 20:47:54 +0200

    moduli29

---
+++ moduli.29/main.js
@@ -0,0 +1,37 @@
+// var programmingLanguage = ["python","c++","html"]
+
+// console.log(programmingLanguage)
+// console.log(programmingLanguage[3])
+
+// programmingLanguage.push("java")
+// console.log(programmingLanguage)
+
+
+// programmingLanguage.pop()
+// console.log(programmingLanguage)
+
+
+// programmingLanguage.shift()
+// console.log(programmingLanguage)
+
+
+// programmingLanguage.unshift("php")
+// console.log(programmingLanguage)
+
+
+
+
+// programmingLanguage.splice(2,1)
+// console.log(programmingLanguage)
+
+console.log(Math.floor(Math.random()*100))           
+
+
+var students =["LumArabi","Drinbullizusi","ReistheMathking"];
+
+var [s1,s3]=students;
+
+console.log(s1);
+                      
+
+console.log(s2);
