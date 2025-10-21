009c3a1d87cf52d7dcb7fcebf9178ddffaeda661
LumBerisha-maker <lumberisha12@outlook.com>
2025-10-21 20:44:18 +0200

    moduli31

---
+++ moduli31/main.js
@@ -0,0 +1,35 @@
+
+
+ //For in
+  
+var person = {fistname: "Marcus", lastName: "Rashford", age: 25};
+
+var text = '';
+
+var z;
+
+for (z in person){
+
+text += person[z];
+
+}
+console.log(text);
+ 
+// For of
+
+var names = ["Marcus","Lumi"];
+
+var y;
+
+for( y of names){
+console.log(y)
+}
+
+var text = 'Javascript'
+
+var 1;
+
+for(1 of text){
+
+console.log(1);
+}
