943fb51bc0eab4a5e2e163f1f3182c443434e6c7
LumBerisha-maker <lumberisha12@outlook.com>
2025-10-23 20:45:14 +0200

    moduli32

---
+++ moduli32/main.js
@@ -0,0 +1,36 @@
+// var text="The most famous school id Digital School";
+// var result=text.search("Digital School");
+
+// document.getElementById('result1').innerHTML = result;
+
+
+// var text="The most famous school id Digital School";
+// var result=text.replace(/Digital School/,"Ismail Qemajli");
+
+// document.getElementById('result2').innerHTML = result;
+
+// var text="lumidrinijorareisisufjani"
+
+// var regex= new RegExp ('jora');
+
+// document.getElementById('result3').innerHTML =regex.test(text);
+
+
+// var text= "My school is the best school of all school";
+// var regex= /s/g;
+
+// document.getElementById('result4').innerHTML = text.match(regex)
+
+
+// var text= "My school is the best school of all school";
+// var regex= /[sol]/g;
+
+// document.getElementById('result4').innerHTML = text.match(regex)
+
+
+
+var text= "My school is the best school of all school 10";
+var regex= /[0-9]/g;
+
+document.getElementById('result4').innerHTML = text.match(regex)
+
