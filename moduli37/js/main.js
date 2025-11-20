eddbf754264813de689a4632f7b2b4e13ad3558a
LumBerisha-maker <lumberisha12@outlook.com>
2025-11-20 20:43:15 +0100

    moduli37

---
+++ moduli37/js/main.js
@@ -0,0 +1,39 @@
+$(window).ready(function(){
+    alert('It is working!!!');
+});
+
+
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
+
+
+
+$('#sort').sortable();
+
+$('sort').sortable();
+$(function(){
+
+    $('#accordion').accordion({  
+        collapsible: true
+
+    });
+       $('#tabs').tabs({  
+        collapsible: true      
+});
+
+
+$(function(){
+
+    $('#accordion').accordion({  
+        collapsible: true
